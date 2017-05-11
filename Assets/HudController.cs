d6326e21b80551e77b049a379777ca1d9e5a4b94
Fernando <fernando@aguilar.net.br>
2017-05-11 08:44:20 -0300

    Adding score, hud, changing backgroud win scene

---
+++ Assets/HudController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour {
+	private int theScore;
+	public Text scoreText;
+	public Text finalScore;
+
+	// Use this for initialization
+	void Start () {
+		theScore = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		theScore++;
+		scoreText.text = "" + theScore;
+	}
+
+	public int getScore(){
+		return this.theScore;
+	}
+}
---
+++ Assets/Scripts/SinglePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SinglePlayer : MonoBehaviour {
+	public GameObject p1;
+
+	public string start;
+	public bool haveOnePlayer = false;
+
+	void Start () {
+		DontDestroyOnLoad (p1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown (start)) {
+			Debug.Log("ok");
+			if (haveOnePlayer) {
+				SceneManager.LoadScene ("Game");
+				p1.GetComponent<SpriteRenderer> ().enabled = false;
+			}
+		}
+
+		if (p1.GetComponent<ChoosedPlayer> ().isOnGame) {
+			haveOnePlayer = true;
+		}
+	}
+}
