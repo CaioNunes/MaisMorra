ba29acc978377675fa59a05a37975612469efec8
Caio Nunes <caiofelipe147@gmail.com>
2017-07-18 20:32:31 -0300

    Editing logic to continue music at Start and Selection scene

---
+++ Assets/Scripts/MenuMusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusicController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (SceneManager.GetActiveScene().name == "Start")
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (SceneManager.GetActiveScene().name != "Start" && SceneManager.GetActiveScene().name != "PlayerSelection")
+        {
+            Debug.Log("Mandei destruir!");
+            Destroy(this.gameObject);
+        }
+	}
+}
