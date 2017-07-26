0f15b3416bc54b6cd1f236f45461a6290ef17ae3
EduardoVentura <eduardopventura@hotmail.com>
2017-07-25 23:24:55 -0300

    Create basic protoype to mode game "blackout"

---
+++ Assets/Scripts/LightsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsTest : MonoBehaviour {
+
+    Light illumination;
+    bool isOn = false;
+    public string turnOn;
+
+	// Use this for initialization
+	void Start () {
+        illumination = GetComponentInChildren<Light>();
+        illumination.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(turnOn)) {
+            if (isOn)
+            {
+                illumination.enabled = false;
+                isOn = false;
+            }
+            else
+            {
+                illumination.enabled = true;
+                isOn = true;
+            }
+                
+        }
+	}
+}
