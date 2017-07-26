1e1b2b5a592607227677f0b686149fc979bda784
EduardoVentura <eduardopventura@hotmail.com>
2017-07-26 20:52:26 -0300

    Criando combate para modo blackout

---
+++ Assets/Scripts/PLayerAttack.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerAttack : MonoBehaviour {
+
+    private float timmerAttackDuration = 0;
+    private float timmerAttack = 0;
+    private bool attacking = false;
+
+    public float attackDuration;
+    public float attackCD;    
+    public string punch;
+
+    public Collider2D attackTrigger;
+    private Animator anim;
+
+
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        //attackTrigger.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetButtonDown(punch) && !attacking)
+        {
+            attacking = true;
+            attackTrigger.enabled = true;
+            timmerAttack = attackCD;
+            timmerAttackDuration = attackDuration;
+
+        }
+
+        if (attacking)
+        {
+            AttackDuration();
+            AttackCD();            
+        }
+
+       
+
+	}
+
+    void AttackDuration()
+    {     
+        if(timmerAttackDuration > 0)
+        {
+            timmerAttackDuration -= Time.deltaTime;                        
+        }else{            
+            //attackTrigger.enabled = false;
+        }
+    }
+
+    void AttackCD()
+    {        
+        if (timmerAttack > 0)
+        {
+            timmerAttack -= Time.deltaTime;            
+        }
+        else
+        {            
+            attacking = false;
+        }
+    }
+}
