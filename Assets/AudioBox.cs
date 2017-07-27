62d05b861f3809e9d5272ac7ba8d04dff1ca358c
Caio Nunes <caiofelipe147@gmail.com>
2017-07-26 23:26:47 -0300

    Criando logica para caixa de áudio

---
+++ Assets/AudioBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBox : MonoBehaviour {
+
+    public AudioClip voice;
+    public AudioClip sound2;
+    private bool colidiu = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+	}
+
+    void OnTriggerEnter2D(Collider2D col) {
+        AudioSource.PlayClipAtPoint(voice, transform.position);
+    }
+}
